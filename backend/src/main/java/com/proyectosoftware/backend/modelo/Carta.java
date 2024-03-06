fb7953aa9e48960249dea11f68393182fd411fe1
Nicolas <841142@unizar.es>
2024-03-06 23:06:57 +0100

    Cambios de legibilidad a la clase Poker

--- backend/src/main/java/com/proyectosoftware/backend/modelo/Carta.java
+++ backend/src/main/java/com/proyectosoftware/backend/modelo/Carta.java
@@ -1,6 +1,40 @@
 package com.proyectosoftware.backend.modelo;
 
 public class Carta {
-    protected int numero;
-    protected int color;
+    private int numero;
+    private int color;
+
+    public Carta(){}
+
+    /**
+     * Devuelve el numero de la carta
+     * @return numero de la carta
+     */
+    public int getNumero(){
+        return numero;
+    }
+    
+    /**
+     * Devuelve el color de la carta
+     * @return color de la carta
+     */
+    public int getColor(){
+        return color;
+    }
+
+    /**
+     * Establece el numero de la carta
+     * @param numero
+     */
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+
+    /**
+     * Establece el color de la carta
+     * @param color
+     */
+    public void setColor(int color){
+        this.color = color;
+    }
 }
--- backend/src/main/java/com/proyectosoftware/backend/modelo/Poker.java
+++ backend/src/main/java/com/proyectosoftware/backend/modelo/Poker.java
@@ -1,48 +1,49 @@
 package com.proyectosoftware.backend.modelo;
 
 public class Poker {
-    protected int id;
-    protected int bote;
-    protected int ultima_apuesta;
-    protected Carta[] cartas_mesa;
-    protected Carta[] mazo;
+    private int id;
+    private int bote;
+    private int ultima_apuesta;
+    private Carta[] cartas_mesa;
+    private Carta[] mazo;
 
     public Poker(int id, int bote, int ultima_apuesta, String mazo, String cartas_mesa) {
         super();
         this.id = id;
         this.bote = bote;
         this.ultima_apuesta = ultima_apuesta;
-        String[] cartas_mazo = mazo.split(";");
+
+        this.mazo = parseStringCartas(mazo);
+        this.cartas_mesa = parseStringCartas(cartas_mesa);
+    }
+
+    /**
+     * 
+     * @param cadenaCartas, contiene la cadena de carecteres recibida desde la Base de Datos
+     * @return retVal, contiene el vector de Carta que representa la baraja de juego
+     */
+    private Carta[] parseStringCartas(String cadenaCartas){
+        Carta[] retVal = new Carta[52];
+        String[] cartas_mazo = cadenaCartas.split(";");
         int i = 0;
+
         for (String carta : cartas_mazo){
             String[] parte_carta = carta.split(",");
             int parte = 0;
+
             for (String info_carta : parte_carta){
                 if (parte == 0) {
-                   this.mazo[i].color =  Integer.parseInt(info_carta);
-                   parte = 1;
+                    int color = Integer.parseInt(info_carta);
+                    retVal[i].setColor(color);
+                    parte = 1;
                 }
-                else if (parte == 1){
-                    this.mazo[i].numero =  Integer.parseInt(info_carta);;
+                else{
+                    int numero = Integer.parseInt(info_carta);
+                    retVal[i].setNumero(numero);
                 }
             }        
             i++;    
         }
-        String[] cartas_m = cartas_mesa.split(";");
-        int j = 0;
-        for (String carta : cartas_m){
-            String[] parte_carta = carta.split(",");
-            int parte = 0;
-            for (String info_carta : parte_carta){
-                if (parte == 0) {
-                   this.cartas_mesa[j].color =  Integer.parseInt(info_carta);
-                   parte = 1;
-                }
-                else if (parte == 1){
-                    this.cartas_mesa[j].numero =  Integer.parseInt(info_carta);;
-                }
-            }        
-            j++;    
-        }
+        return retVal;
     }
 }
