ef9de789197c16df37048da9ceb6027cf7fed94f
jorge1467 <91035401+jorge1467@users.noreply.github.com>
2024-03-07 12:48:29 +0100

    Atributos UNO

--- backend/src/main/java/com/proyectosoftware/backend/modelo/UNO.java
+++ backend/src/main/java/com/proyectosoftware/backend/modelo/UNO.java
@@ -1,5 +1,16 @@
 package com.proyectosoftware.backend.modelo;
 
 public class UNO {
-    
+     private int id;
+     private int sentido;
+     private Carta[] mazo;
+     private Carta[] ultimaCarta;
+     
+     public UNO(int id, int sentido, String mazo, String ultimaCarta){
+         
+         this.id =id;
+         this.sentido=sentido;
+         this.mazo=mazo;
+         this.ultimaCarta=ultimaCarta;
+     }
 }
