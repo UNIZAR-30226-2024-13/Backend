951b1954fca8d470574f77812189cd3db77803e3
812019 <812019@unizar.es>
2024-03-13 00:40:34 +0100

    Estructura de calses base implementada

---
+++ backend/src/main/java/com/proyectosoftware/backend/modelo/interfaces/Baraja.java
@@ -0,0 +1,70 @@
+package com.proyectosoftware.backend.modelo.interfaces;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+import com.proyectosoftware.backend.modelo.Carta;
+
+/**
+ * Baraja abstracta
+ */
+public interface Baraja {
+   
+    default List<Carta> parsearCartas(String cartasBaraja){
+        List<Carta> cartas = new ArrayList<>();
+        for (String carta : cartasBaraja.split(";")) {
+            String[] datosCarta = carta.split(",");
+            int color = Integer.parseInt(datosCarta[0]);
+            int numero = Integer.parseInt(datosCarta[1]); 
+            if(esCartaDeLaBaraja(numero, color)){
+                cartas.add(new Carta(numero, color));
+            }else {
+                /**
+                 * TODO: lanzar error
+                 */
+            }
+        }
+        return cartas;
+    }
+
+    /**
+     * 
+     * @param carta - carta para verificar
+     * @return {@code true} si la carta pertenece a la baraja
+     */
+    public boolean esCartaDeLaBaraja(Carta carta);
+
+    /**
+     * 
+     * @param color     - color de la carta a verificar
+     * @param numero    - numero de la carta a verificar
+     * @return {@code true} si la carta pertenece a la baraja  
+     */
+    public boolean esCartaDeLaBaraja(int color, int numero);
+
+    /**
+     * 
+     * @param color - Codigo del color se quiere interpretar
+     * @return Devuelve el color real
+     */
+    public String colorReal(int color);
+
+    /**
+     * 
+     * @param numero - Codigo del numero que se quiere interpretar
+     * @return Devuelve el numero real
+     */
+    public String numeroReal(int numero);
+
+    /**
+     * 
+     * @return - Devuelve una copia de las cartas de la baraja
+     */
+    public List<Carta> devolverCartas();
+    
+}
--- backend/src/main/java/com/proyectosoftware/backend/modelo/juegos/Mentiroso.java
+++ backend/src/main/java/com/proyectosoftware/backend/modelo/juegos/Mentiroso.java
@@ -1,16 +1,49 @@
 package com.proyectosoftware.backend.modelo.juegos;
 
-import com.proyectosoftware.backend.modelo.Carta;
-import com.proyectosoftware.backend.modelo.Partida;
+import com.proyectosoftware.backend.modelo.interfaces.Baraja;
+import com.proyectosoftware.backend.modelo.interfaces.Estado;
 import com.proyectosoftware.backend.modelo.interfaces.JuegoSinApuesta;
 
 public class Mentiroso implements JuegoSinApuesta{
-    private Carta[] cartas_mesa;
+    private Baraja baraja;
 
-    public Mentiroso(int id, int turno, String cartas_mesa) {
-        super(id, turno);
-        Carta carta = new Carta();
+    public Mentiroso() {
+        
+    }
 
-        this.cartas_mesa = carta.parseStringCartas(cartas_mesa);
+    public Mentiroso(Estado estado) {
+        
+    }
+
+    @Override
+    public Estado guardar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardar'");
+    }
+
+    @Override
+    public void cargar(Estado estado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cargar'");
+    }
+
+    @Override
+    public Estado recuperarEstado(String estadoString) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recuperarEstado'");
+    }
+
+    @Override
+    public String crearEstado(Estado estado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearEstado'");
+    }
+
+
+
+    @Override
+    public void siguenteTurno() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'siguenteTurno'");
     }
 }
