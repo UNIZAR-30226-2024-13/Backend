421c424b607080c25dfd0206207b401eb8e29c42
Kmc1319 <158459353+Kmc1319@users.noreply.github.com>
2024-03-13 11:24:22 +0100

    Modelo Poker y BJ

--- backend/src/main/java/com/proyectosoftware/backend/modelo/juegos/BlackJack.java
+++ backend/src/main/java/com/proyectosoftware/backend/modelo/juegos/BlackJack.java
@@ -1,18 +1,77 @@
 package com.proyectosoftware.backend.modelo.juegos;
 
+import java.util.List;
+
 import com.proyectosoftware.backend.modelo.Carta;
 import com.proyectosoftware.backend.modelo.Partida;
+import com.proyectosoftware.backend.modelo.Usuario;
+import com.proyectosoftware.backend.modelo.barajas.BarajaFrancesa;
+import com.proyectosoftware.backend.modelo.interfaces.Baraja;
+import com.proyectosoftware.backend.modelo.interfaces.Estado;
 import com.proyectosoftware.backend.modelo.interfaces.JuegoConApuesta;
 
+/**
+ * Juego del blackjack
+ */
 public class BlackJack implements JuegoConApuesta{
-    private Carta[] cartas_banca;
-    private Carta[] mazo;
+    private List<Carta> cartas_banca;
+    private List<Carta> mazo;
+    private Baraja baraja;
 
+    /**
+     * Constructor por defecto
+     */
     public BlackJack(int id, int turno, String mazo, String cartas_banca) {
         super(id, turno);
-        Carta carta = new Carta();
+        baraja = BarajaFrancesa.devolverInstancia();
+        this.mazo = baraja.parsearCartas(mazo);
+        this.cartas_banca = baraja.parsearCartas(cartas_banca);
+        
+    }
 
-        this.mazo = carta.parseStringCartas(mazo);
-        this.cartas_banca = carta.parseStringCartas(cartas_banca);
+    /**
+     * Cargar un juego de blackjack dado un estado
+     * @param estado
+     */
+    public BlackJack(Estado estado){
+
+    }
+
+    @Override
+    public Estado guardar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardar'");
+    }
+
+    @Override
+    public void cargar(Estado estado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cargar'");
+    }
+
+    @Override
+    public Estado recuperarEstado(String estadoString) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recuperarEstado'");
+    }
+
+    @Override
+    public String crearEstado(Estado estado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearEstado'");
+    }
+
+
+    @Override
+    public void siguenteTurno() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'siguenteTurno'");
+    }
+
+
+    @Override
+    public void apostar(Usuario usaurio, double apuesta){
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'apostar'");
     }
 }
--- backend/src/main/java/com/proyectosoftware/backend/modelo/juegos/Poker.java
+++ backend/src/main/java/com/proyectosoftware/backend/modelo/juegos/Poker.java
@@ -1,22 +1,81 @@
 package com.proyectosoftware.backend.modelo.juegos;
 
+import java.util.List;
+
 import com.proyectosoftware.backend.modelo.Carta;
 import com.proyectosoftware.backend.modelo.Partida;
+import com.proyectosoftware.backend.modelo.Usuario;
+import com.proyectosoftware.backend.modelo.barajas.BarajaFrancesa;
+import com.proyectosoftware.backend.modelo.interfaces.Baraja;
+import com.proyectosoftware.backend.modelo.interfaces.Estado;
 import com.proyectosoftware.backend.modelo.interfaces.JuegoConApuesta;
 
+/**
+ * Juego del poker
+ */
 public class Poker implements JuegoConApuesta{
     private int bote;
     private int ultima_apuesta;
-    private Carta[] cartas_mesa;
-    private Carta[] mazo;
+    private List<Carta> cartas_mesa;
+    private List<Carta> mazo;
+    private Baraja baraja;
 
+    /**
+     * Constructor por defecto
+     */
     public Poker(int id, int turno, int bote, int ultima_apuesta, String mazo, String cartas_mesa) {
         super(id, turno);
         this.bote = bote;
         this.ultima_apuesta = ultima_apuesta;
-        Carta carta = new Carta();
+        baraja = BarajaFrancesa.devolverInstancia();
+        this.mazo = baraja.parsearCartas(mazo);
+        this.cartas_mesa = baraja.parsearCartas(cartas_mesa);
+        
+    }
 
-        this.mazo = carta.parseStringCartas(mazo);
-        this.cartas_mesa = carta.parseStringCartas(cartas_mesa);
+    /**
+     * Cargar un juego de poker dado un estado
+     * @param estado
+     */
+    public Poker(Estado estado){
+
+    }
+
+    @Override
+    public Estado guardar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardar'");
+    }
+
+    @Override
+    public void cargar(Estado estado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cargar'");
+    }
+
+    @Override
+    public Estado recuperarEstado(String estadoString) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recuperarEstado'");
+    }
+
+    @Override
+    public String crearEstado(Estado estado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearEstado'");
+    }
+
+
+    @Override
+    public void siguenteTurno() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'siguenteTurno'");
+    }
+
+
+    @Override
+    public void apostar(Usuario usaurio, double apuesta){
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'apostar'");
     }
 }
