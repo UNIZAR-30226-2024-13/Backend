cbee0b14fb2fe329ca5c68006d00db4e3a738d8d
Yorchy <143226087+Iortxi@users.noreply.github.com>
2024-03-27 19:27:17 +0100

    Funciones turnoCroupier, valorMano y valorCarta

--- backend/src/main/java/com/proyectosoftware/backend/modelo/juegos/BlackJack.java
+++ backend/src/main/java/com/proyectosoftware/backend/modelo/juegos/BlackJack.java
@@ -1,6 +1,7 @@
 package com.proyectosoftware.backend.modelo.juegos;
 
 import java.util.List;
+import java.util.HashMap;
 
 import com.proyectosoftware.backend.modelo.Carta;
 import com.proyectosoftware.backend.modelo.Partida;
@@ -21,7 +22,7 @@ public class BlackJack implements JuegoConApuesta, Estado {
     private Map<String, Integer> fichas_usuario;        // Diccionario con los usuarios y sus fichas a usar en la partida
     private Map<String, List<Carta>> cartas_usuario;    // Diccionario con los usuarios y sus cartas a usar en la partida
     private Map<String, Boolean> plantado;              // Diccionario con los usuarios y si se han plantado o no (inicializar a false)
-
+    private List<Carta> cartas_croupier;                // Lista de cartas del croupier (Carta de índice 0 está boca arriba y la otra boca abajo)
 
     /*
      * Constructor por defecto
@@ -33,6 +34,7 @@ public class BlackJack implements JuegoConApuesta, Estado {
         fichas_usuario = new HashMap<>();
         cartas_usuario = new HashMap<>();
         plantado = new HashMap<>();
+        cartas_croupier = new ArrayList<>();
     }
 
 
@@ -129,9 +131,51 @@ public class BlackJack implements JuegoConApuesta, Estado {
      * @param usuario - Usuario que se planta
     */
     public void plantarse(Usuario usuario) {
-        plantados.put(usuario.getId(), true)
+        plantados.put(usuario.getId(), true);
     }
     
+
+    /**
+     * Devuelve el valor de una carta dentro del Blackjack
+     * @param carta - Carta a evaluar
+     * @return valor - Valor de la carta
+    */
+    public int valorCarta(Carta carta) {
+        int numero = carta.getNumero();
+        return (numero == 1) ? 11 : carta;      // Si la carta es un as (valor 1), devuelve 11, sino su valor normal
+    }
+
+
+    /**
+     * Devuelve el valor de una mano dentro del Blackjack
+     * @param cartas - Cartas a evaluar
+     * @return valor - Valor de la mano
+    */
+    public int valorMano(List<Carta> cartas) {
+        int suma = 0;
+        for (carta : cartas) {
+            suma += valorCarta(carta);
+        }
+        return suma;
+    }
+
+
+    /**
+     * El croupier juega su turno después de que
+     * los jugadores hayan jugado sus turnos.
+     * La funcion acaba con la situación final de las cartas del croupier
+    */
+    public void turnoCroupier() {
+        // Se revela la segunda carta del croupier (vista)
+        int suma_croupier = valorMano(cartas_croupier);
+        while (suma_croupier <= 16) {
+            Carta carta_sacada = mazo.get(0);
+            mazo.remove(0);                                         // Se saca una carta y se quita del mazo
+            cartas_croupier.add(carta_sacada);
+            suma_croupier = valorMano(cartas_croupier);
+        }
+    }
+
 
     /**
      * Aumenta el numero de fichas de un usuario
@@ -177,6 +221,10 @@ public class BlackJack implements JuegoConApuesta, Estado {
                 mazo.remove(0);
             }
             cartas_usuario.put(usuarios.get(i).getId(), cartas.clone());
+        }
+        for (int i = 0; i < 2; i++) {                   // Reparto de cartas al croupier
+            cartas_croupier.add(mazo.get(0));
+            mazo.remove(0);
         }
     }
 }
