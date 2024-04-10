bd60e324e042582a50d667a5270b489e799f7efa
Nicolas <841142@unizar.es>
2024-04-10 23:20:42 +0200

    Test automaticos Cinquillo

--- backend/src/test/java/com/proyectosoftware/backend/modelo/juegos/CinquilloTest.java
+++ backend/src/test/java/com/proyectosoftware/backend/modelo/juegos/CinquilloTest.java
@@ -9,14 +9,14 @@ import org.junit.jupiter.api.Test;
 import com.proyectosoftware.backend.modelo.Usuario;
 import com.proyectosoftware.backend.modelo.interfaces.Juego;
 
-public class MentirosoTest {
+public class CinquilloTest {
     private Usuario usuario1;
     private Usuario usuario2;
     private Usuario usuario3;
     private Usuario usuario4;
 
     private Juego juego;
-    private Mentiroso mentiroso;
+    private Cinquillo cinquillo;
 
     @BeforeEach
     public void setup(){
@@ -24,7 +24,7 @@ public class MentirosoTest {
         this.usuario2 = new Usuario("2","2", 0,"2");
         this.usuario3 = new Usuario("3","3", 0,"3");
         this.usuario4 = new Usuario("4","4", 0,"4");
-        this.juego = new Mentiroso();
+        this.juego = new Cinquillo();
     }
 
     @Test
@@ -34,23 +34,27 @@ public class MentirosoTest {
         juego.nuevoUsuario(usuario3);
         juego.nuevoUsuario(usuario4);
 
-        mentiroso = ((Mentiroso)juego);
+        cinquillo = ((Cinquillo)juego);
 
-        JSONObject json = mentiroso.guardar();
+        JSONObject json = cinquillo.guardar();
 
         Assertions.assertThat(((String)json.get("ID"))).startsWith("juego-");
         System.out.println(((String)json.get("ID")));
 
         Assertions.assertThat(json.get("turno")).isEqualTo(0);
-        Assertions.assertThat(json.get("cartas_mesa")).isEqualTo("");
-        Assertions.assertThat(json.get("ultimas_cartas")).isEqualTo(0);
-        Assertions.assertThat(json.get("numero_actual")).isEqualTo(-1);
+        Assertions.assertThat(json.get("primer_jugador")).isIn(0,1,2,3);
         JSONArray usuarios = (JSONArray)json.get("usuarios");
         for (Object object : usuarios) {
             JSONObject usuario = (JSONObject)object;
             Assertions.assertThat(usuario.get("ID")).asString().startsWith("Usuario-");
             Assertions.assertThat(usuario.get("turno_en_juego")).isIn(0,1,2,3);
             Assertions.assertThat(usuario.get("cartas")).asString().matches("([0-9]+,[0-9]+(;[0-9]+,[0-9]+)*)?");
+        }
+        JSONArray escaleras = (JSONArray)json.get("escaleras");
+        for (Object object : escaleras) {
+            JSONObject escalera = (JSONObject)object;
+            Assertions.assertThat(escalera.get("palo")).asString().matches("(OROS)?(COPAS)?(BASTOS)?(ESPADAS)?");
+            Assertions.assertThat(escalera.get("cartas")).asString().matches("([0-9]+,[0-9]+(;[0-9]+,[0-9]+)*)?");
         }
     }
 
@@ -61,19 +65,17 @@ public class MentirosoTest {
         juego.nuevoUsuario(usuario3);
         juego.nuevoUsuario(usuario4);
 
-        mentiroso = ((Mentiroso)juego);
+        cinquillo = ((Cinquillo)juego);
 
-        JSONObject json = mentiroso.guardar();
+        JSONObject json = cinquillo.guardar();
 
-        mentiroso = new Mentiroso(json);
+        cinquillo = new Cinquillo(json);
 
         Assertions.assertThat(((String)json.get("ID"))).startsWith("juego-");
         System.out.println(((String)json.get("ID")));
 
         Assertions.assertThat(json.get("turno")).isEqualTo(0);
-        Assertions.assertThat(json.get("cartas_mesa")).isEqualTo("");
-        Assertions.assertThat(json.get("ultimas_cartas")).isEqualTo(0);
-        Assertions.assertThat(json.get("numero_actual")).isEqualTo(-1);
+        Assertions.assertThat(json.get("primer_jugador")).isIn(0,1,2,3);
 
         JSONArray usuarios = (JSONArray)json.get("usuarios");
         for (Object object : usuarios) {
@@ -81,6 +83,12 @@ public class MentirosoTest {
             Assertions.assertThat(usuario.get("ID")).asString().startsWith("Usuario-");
             Assertions.assertThat(usuario.get("turno_en_juego")).isIn(0,1,2,3);
             Assertions.assertThat(usuario.get("cartas")).asString().matches("([0-9]+,[0-9]+(;[0-9]+,[0-9]+)*)?");
+        }
+        JSONArray escaleras = (JSONArray)json.get("escaleras");
+        for (Object object : escaleras) {
+            JSONObject escalera = (JSONObject)object;
+            Assertions.assertThat(escalera.get("palo")).asString().matches("(OROS)?(COPAS)?(BASTOS)?(ESPADAS)?");
+            Assertions.assertThat(escalera.get("cartas")).asString().matches("([0-9]+,[0-9]+(;[0-9]+,[0-9]+)*)?");
         }
     }
 }
