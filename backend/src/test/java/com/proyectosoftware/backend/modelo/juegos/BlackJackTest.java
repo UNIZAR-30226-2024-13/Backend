25e90668e4e7995350dcb6e2960e0162499065d0
Iortxi <jortuaito11@gmail.com>
2024-04-11 10:35:10 +0200

    Test Blackjack vacío

---
+++ backend/src/test/java/com/proyectosoftware/backend/modelo/juegos/BlackJackTest.java
@@ -0,0 +1,15 @@
+package com.proyectosoftware.backend.modelo.juegos;
+
+import org.junit.jupiter.api.Test;
+
+public class BlackJackTest {
+    @Test
+    void testCargar() {
+
+    }
+
+    @Test
+    void testGuardar() {
+
+    }
+}
